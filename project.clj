fb8d2e29bdf796de59f0514d8e70552c2df3ba6b
Julien Eluard <julien.eluard@gmail.com>
2013-05-16 11:34:59 -0300

    Added ring wrapper.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :sub ["ring"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.webjars/webjars-locator "0.3"]
-                 [org.webjars/rickshaw "1.1.2-1"]
+                 [org.webjars/webjars-locator "0.4-SNAPSHOT"]
                  [ring/ring-core "1.2.0-beta1"]]
   :plugins [[lein-sub "0.2.4"]])
--- src/clj_webjars.clj
+++ src/clj_webjars.clj
@@ -1,11 +1,17 @@
 (ns clj-webjars
-  (:require [ring.middleware.file-info :refer [make-http-format]]
-            [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.string :refer [replace-first]]
+            [ring.util.request :as request]
+            [ring.util.response :as response]
+            [ring.middleware.file-info :as file-info])
   (:import [org.webjars WebJarAssetLocator]))
 
-(set! *warn-on-reflection* true)
-
 (def ^:dynamic ^WebJarAssetLocator *locator* (WebJarAssetLocator.))
+
+;;Inspired by https://gist.github.com/cemerick/3655445
+
+(defn locate-asset [path]
+  (.getFullPath *locator* path))
 
 (defn list-assets
   ([] (list-assets "/"))
@@ -27,10 +33,60 @@
        java.util.Date.))
 
 (defn- date-as-string [^java.util.Date date]
-  (.format (make-http-format) date))
+  (.format (file-info/make-http-format) date))
 
 (defn load-assets []
   (into {} (for [asset (list-assets)]
              [asset (let [url (load-resource asset)]
-                      (with-open [^java.io.InputStream stream (io/input-stream url)]
-                        {:content stream :last-modified (last-modified url)}))])))
+                      (let [^java.io.InputStream stream (io/input-stream url)]
+                        {:stream stream :last-modified (last-modified url)}))])))
+
+(def assets (atom {}))
+
+(defn refresh-assets! []
+  (reset! assets (load-assets)))
+
+(refresh-assets!)
+
+(defn- response-not-modified []
+  (-> (response/response "")
+      (response/status 304)
+      (response/header "Content-Length" 0)
+      (response/header "Date" (date-as-string (java.util.Date.)))))
+
+(defn- response-modified [stream date]
+  (-> (response/response stream)
+      (response/header "Last-Modified" (date-as-string date))))
+
+(defn response-multiple-matches [e]
+  (-> (response/response (.getMessage e))
+      (response/status 400)))
+
+(defn remove-leading-slash [^String string]
+  (if (.startsWith string "/")
+    (subs string 1)
+    string))
+
+(defn remove-trailing-slash [^String string]
+  (if (.endsWith string "/")
+    (subs string 0 (- (.length string) 1))
+    string))
+
+(defn extract-path [^String uri roots]
+  (some #(when (.startsWith (remove-leading-slash uri) %) (remove-trailing-slash (replace-first uri % ""))) roots))
+
+(defn get-asset [uri roots]
+  (if-let [path (extract-path uri (map #(remove-leading-slash %) roots))]
+    (get @assets (locate-asset path))))
+
+(defn asset-response [req roots]
+  (try (if-let [asset (get-asset (request/path-info req) roots)]
+    (let [last-modified (:last-modified asset)]
+      (if (#'file-info/not-modified-since? req last-modified)
+        (response-not-modified)
+        (response-modified (:stream asset) last-modified))))
+      (catch IllegalArgumentException e (response-multiple-matches e))))
+
+(defn wrap-webjars
+  ([handler] (wrap-webjars handler ["assets/js/" "assets/css/"]))
+  ([handler roots] (fn [req] (let [response (asset-response req roots)] (if (nil? response) (handler req) response)))))
