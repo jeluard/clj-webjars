5c466925d66afffc250bb985495855126ac1867f
Julien Eluard <julien.eluard@gmail.com>
2013-05-17 14:12:18 -0300

    Updated ring-core version.

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.webjars/webjars-locator "0.4"]
-                 [ring/ring-core "1.2.0-beta1"]
+                 [ring/ring-core "1.2.0-beta2"]
                  [org.webjars/rickshaw "1.1.2-1" :scope "test"]])
