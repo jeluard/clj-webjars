4ee6e79c317b0eb0a763cc6e5c9bb9d48b00e9b1
Julien Eluard <julien.eluard@gmail.com>
2013-05-16 23:08:41 -0300

    Removed ring sub-project.

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :url "http://github.com/jeluard/clj-webjars"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :sub ["ring"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.webjars/webjars-locator "0.4-SNAPSHOT"]
-                 [ring/ring-core "1.2.0-beta1"]]
-  :plugins [[lein-sub "0.2.4"]])
+                 [ring/ring-core "1.2.0-beta1"]])
